64f64f90b222346e306a2121975e3b4074c98480
phuc1nguyen <phuc.ng13988@gmail.com>
2022-10-21 17:21:00 +0700

    Add popup form to add book to library

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
 const buttonAdd = document.querySelector(".button-add");
+const popupContainer = document.querySelector(".popup-form");
+const formAddButton = document.querySelector(".form-add button");
 const thisYear = new Date().getFullYear();
 let myLib = [];
 
 document.querySelector(".footer-year").textContent = thisYear;
-buttonAdd.addEventListener("click", addBookToLibrary);
+buttonAdd.addEventListener("click", showAddForm);
+formAddButton.addEventListener("click", () => {
+  console.log(12345);
+});
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("overlay")) {
+    popupContainer.style.display = "none";
+  }
+});
+window.addEventListener("keydown", (e) => {
+  if (e.code === "Escape") {
+    popupContainer.style.display = "none";
+  }
+});
 
 function Book(title, author, pages, isRead) {
   this.title = title;
@@ -17,4 +32,8 @@ function addBookToLibrary() {
 
   myLib.push(newBook);
   console.log(myLib);
+}
+
+function showAddForm() {
+  popupContainer.style.display = "flex";
 }
